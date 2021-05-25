ca5418c6c0dd45d347929f60067875fcf347c246
Dilili <miniaoe@126.com>
2021-05-25 14:01:31 +0800

    docs

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vitePluginVuedoc, { vueDocFiles } from "vite-plugin-vuedoc";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
+  base: "pure-ui",
   plugins: [
     vitePluginVuedoc({ wrapperClass: "markdown-body" }),
     vue({ include: [...vueDocFiles] }),
